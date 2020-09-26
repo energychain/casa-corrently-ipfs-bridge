514e8990cac87e46f4a3095bfd342a4118ded289
Thorsten Zoerner <me@thorsten-zoerner.com>
2020-09-26 03:44:31 +0200

    Auto Commit

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 module.exports = function(config) {
     return {
         broadcast: async function(msgcids) {
-          const BRIDGE = require("ipfs-http-client");
-          const bridge = BRIDGE('http://localhost:5001');
-          await bridge.files.write('/p2p',JSON.stringify(msgcids),{create:true,parents:true});
-          const stats = await bridge.files.stat("/",{hash:true});
-          const lhash = await bridge.name.publish('/ipfs/'+stats.cid.toString());
-          console.log("Broadcast published",lhash);
+          let lhash = '';
+          try {
+            const BRIDGE = require("ipfs-http-client");
+            const bridge = BRIDGE('http://localhost:5001');
+            await bridge.files.write('/p2p',JSON.stringify(msgcids),{create:true,parents:true});
+            const stats = await bridge.files.stat("/",{hash:true});
+            lhash = await bridge.name.publish('/ipfs/'+stats.cid.toString());
+            console.log("Broadcast published",lhash);
+          } catch(e) {
+            console.log(e);
+          }
           return lhash;
         }
     }
